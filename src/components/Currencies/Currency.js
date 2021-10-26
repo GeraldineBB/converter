55d9dc44721ca7dfe79f164f9f4a13c49edddc5e
GeraldineBB <geraldine-bornet@hotmail.fr>
2021-10-26 22:38:00 +0200

    CSS currencies V2

--- src/components/Currencies/Currency.js
+++ src/components/Currencies/Currency.js
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 import './styles.scss';
 
 const Currency = ({ name }) => (
-  <li className="content__item">{name}</li>
+  <li className="content__item"><span className="content__text">{name}</span></li>
 );
 
 Currency.propTypes = {
