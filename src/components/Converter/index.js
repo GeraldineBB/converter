af712606ee2ed9c394cac0b6694c8973ee085773
GeraldineBB <geraldine-bornet@hotmail.fr>
2021-10-27 09:51:02 +0200

    Variables scss

--- src/components/Converter/index.js
+++ src/components/Converter/index.js
@@ -1,27 +1,20 @@
 // Import des composants
-import Header from '../Header';
-import Currencies from '../Currencies';
-import Results from '../Results';
 
 // == Import
 import './styles.scss';
 
 // Import des données
-import currenciesData from '../../data/currencies';
+// import currenciesData from '../../data/currencies';
 
 // == Composant
 const Converter = () => (
 
   <div className="converter">
-
-    <div className="converter__container">
-      <Header />
-      <Currencies currencies={currenciesData} />
-      <Results />
-
-    </div>
-
+    <div>Amount</div>
+    <div>Currencies</div>
+    <div>ConvertedAmount</div>
   </div>
+
 
 );
 
